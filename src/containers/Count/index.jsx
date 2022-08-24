26d972b56397e15bd690cdf604129a0cd30434b7
steve zhang <871506263@qq.com>
2022-08-24 21:58:44 +0800

    提交代码于: 2022-08-24 21:58:44

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -10,7 +10,7 @@ import {
 //引入connect用于连接UI组件与redux
 import {connect} from 'react-redux'
 
-/* 
+/*
 	1.mapStateToProps函数返回的是一个对象；
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapStateToProps用于传递状态
@@ -19,7 +19,7 @@ function mapStateToProps(state){
 	return {count:state}
 }
 
-/* 
+/*
 	1.mapDispatchToProps函数返回的是一个对象；
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapDispatchToProps用于传递操作状态的方法
@@ -34,4 +34,3 @@ function mapDispatchToProps(dispatch){
 
 //使用connect()()创建并暴露一个Count的容器组件
 export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
-
